19d7738d03d4e8b0e8d617619e67383c1bfc92b3
lior Shine <shinelior@gmail.com>
2019-12-20 01:40:47 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -1,11 +1,52 @@
-BASE_HOA = {
+
+#def sub_show(show_array)
+   #return show_array
+ # end
+  
+#def subb_shoe(show_array)
+ # return show_array[1]
+ #end
+ 
+#def subsub_shoe(show_array)
+ # return show_array[2]
+ #end
+ 
+ 
+#base_hoa = {
+ # :chipmunks => ["Alvin", "Simon", "Theodore"],
+  #:third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
+ # :jetsons => ["George", "Jane", "Judy", "Elroy"]
+#}
+
+
+  # Write your implementation here
+  # Should return the array of the 'show' argument
+  #def add_character(show, name)
+  
+  
+  def add_character(show_array,name="")
+  base_hoa = {
   :chipmunks => ["Alvin", "Simon", "Theodore"],
-  :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
+ :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
   :jetsons => ["George", "Jane", "Judy", "Elroy"]
 }
 
+ show_array=[]
+ 
+ base_hoa[:chipmunks] << "Dave"
+ show_array << base_hoa[:chipmunks]
+ base_hoa[:third_earthers] << "Snarf"
+ show_array << base_hoa[:third_earthers]
+  base_hoa[:jetsons] << "Astro"
+  show_array << base_hoa[:jetsons]
+  
+  #base_hoa[:jetsons]
+   
+  
 
-def add_character(show, name)
-  # Write your implementation here
-  # Should return the array of the 'show' argument
+ return show_array
+ 
 end
+
+#end
+
